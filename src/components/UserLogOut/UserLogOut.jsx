ef699d8b66b4a222fe936322d20bb43f3b133057
teyotobias <teyotobias@utexas.edu>
2023-08-16 16:37:07 -0500

    btn shape

--- src/components/UserLogOut/UserLogOut.jsx
+++ src/components/UserLogOut/UserLogOut.jsx
@@ -9,7 +9,7 @@ export default function UserLogOut({ user, setUser }) {
 
   return (
     <div className="UserLogOut">
-      <div>{user.name}</div>
+      <div className="usr-txt">{user.name}</div>
       <div className="email">{user.email}</div>
       <button className="btn-sm logOutBtn" onClick={handleLogOut}>LOG OUT</button>
     </div>
