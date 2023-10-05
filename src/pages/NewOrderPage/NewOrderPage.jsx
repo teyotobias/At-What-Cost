a897ab38bc3011370f972e684accbe503656a792
teyotobias <teyotobias@utexas.edu>
2023-10-04 21:46:18 -0500

    Responsive styling initiated

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -13,9 +13,21 @@ export default function NewOrderPage({ user, setUser}) {
     const [storeItems, setStoreItems] = useState([]);
     const [activeCat, setActiveCat] = useState('');
     const [cart, setCart] = useState(null);
+    const [isLeftSidebarOpen, setIsLeftSidebarOpen] = useState(false);
+    const [isRightSidebarOpen, setIsRightSidebarOpen] = useState(false);
     const categoriesRef = useRef([]);
     //navigate fx to change routes programmatically
     const navigate = useNavigate();
+
+    const toggleLeftSidebar = () => {
+        setIsRightSidebarOpen(false);
+        setIsLeftSidebarOpen(!isLeftSidebarOpen);
+    }
+    
+    const toggleRightSidebar = () => {
+        setIsLeftSidebarOpen(false);
+        setIsRightSidebarOpen(!isRightSidebarOpen);
+    }
 
 
     useEffect(function() {
@@ -53,8 +65,15 @@ export default function NewOrderPage({ user, setUser}) {
 
 
     return (
-        <main className="NewOrderPage">
-            <aside>
+        <main className={`NewOrderPage ${isLeftSidebarOpen ? 'left-sidebar-open' : ''} ${isRightSidebarOpen ? 'right-sidebar-open' : ''}`}>
+            <aside className={`left-sidebar ${isLeftSidebarOpen ? 'left-sidebar-open' : 'left-sidebar-closed'}`}>
+                {/* Arrow Button */}
+                <button 
+                    className={`arrow-btn arrow-${isLeftSidebarOpen ? 'right' : 'left'}`} 
+                    onClick={toggleLeftSidebar}
+                    >
+                    {isLeftSidebarOpen ? '❮' : '❯'}
+                </button>
                 <Logo />
                 <CategoryList
                 categories={categoriesRef.current}
@@ -64,11 +83,23 @@ export default function NewOrderPage({ user, setUser}) {
                 <Link to="/orders" className="button btn-sm histBtn">PREVIOUS ORDERS</Link>
                 <UserLogOut user={user} setUser={setUser} />
             </aside>
-            <StoreList
+            <section className="main-content">
+                <StoreList
                 storeItems={storeItems.filter(item => item.category.name === activeCat)}
                 handleAddToOrder={handleAddToOrder}
-            />
-            <OrderDetail order={cart} handleChangeQty={handleChangeQty} handleSuccessfulPayment={handleSuccessfulPayment}/>
+                />
+            </section>
+        
+            <aside className={`right-sidebar ${isRightSidebarOpen ? 'right-sidebar-open' : 'right-sidebar-closed'}`}>
+                {/* Arrow Button */}
+                <button 
+                    className={`arrow-btn arrow-${isRightSidebarOpen ? 'left' : 'right'}`} 
+                    onClick={toggleRightSidebar}
+                    >
+                    {isRightSidebarOpen ? '❯' : '❮'}
+                </button>
+                <OrderDetail order={cart} handleChangeQty={handleChangeQty} handleSuccessfulPayment={handleSuccessfulPayment}/>
+            </aside>
         </main>
     )
 }
