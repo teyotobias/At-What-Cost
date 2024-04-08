08622fe48f422a3e5ce42f25aa254a3da0ad9525
teyotobias <teyotobias@utexas.edu>
2024-04-07 22:06:12 -0500

    Toastify for successful payment instead of modal.

--- src/pages/PaymentSuccessPage/PaymentSuccessPage.jsx
+++ src/pages/PaymentSuccessPage/PaymentSuccessPage.jsx
@@ -2,12 +2,34 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { verifySession } from '../../utilities/orders-api';
-import CustomModal from '../../components/CustomModal/CustomModal';
+import { toast } from 'react-toastify';
+import './PaymentSuccessPage.css';
+import Logo from '../../components/Logo/Logo';
 export default function PaymentSuccessPage() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const sessionId = searchParams.get('session_id');
-  const [showModal, setShowModal] = useState(false);
+
+  // useEffect(() => {
+  //   const verifyPayment = async () => {
+  //     if (sessionId) {
+  //       try {
+  //         const response = await verifySession(sessionId);
+  //         if (response.verified) {
+  //           setShowModal(true);
+  //         } else {
+  //           navigate('/orders/new');
+  //         }
+  //       } catch (error) {
+  //         console.error('Verification failed: ', error);
+  //         navigate('/orders/new')
+  //       }
+  //     }
+  //   };
+
+  //   verifyPayment();
+  // }, [sessionId, navigate]);
+
 
   useEffect(() => {
     const verifyPayment = async () => {
@@ -15,13 +37,32 @@ export default function PaymentSuccessPage() {
         try {
           const response = await verifySession(sessionId);
           if (response.verified) {
-            setShowModal(true);
+            // Call the toast function with a custom render component
+
+            const toastId = 'paymentSuccessId';
+            toast(<div className="toastContainer">
+              Thank You! 
+              <button onClick={() => {
+                toast.dismiss(toastId);
+                navigate('/orders');
+               }} 
+               style={{ display: 'block', marginTop: '10px', color: 'rgb(102,102,102', background: 'rgb(206,251,244)', cursor: 'pointer' }}>
+                Go to Orders
+              </button>
+            </div>, {
+              position: "bottom-center",
+              autoClose: false,
+              closeOnClick: false,
+              draggable: true,
+              toastId: toastId, // Ensure unique ID
+              style: { marginBottom: 'clamp(5px, 5vh, 10px'},
+            });
           } else {
             navigate('/orders/new');
           }
         } catch (error) {
           console.error('Verification failed: ', error);
-          navigate('/orders/new')
+          navigate('/orders/new');
         }
       }
     };
@@ -30,19 +71,15 @@ export default function PaymentSuccessPage() {
   }, [sessionId, navigate]);
 
   return (
-    <div>
-      Verifying your payment...
-      {showModal && (
-        <CustomModal
-          message="Payment Successful!"
-          onClose={() => {
-            setShowModal(false); // Hide modal on close
-            navigate('/orders'); // Navigate to Order History
-          }}
-          closeMessage="Order History"
-        />
-      )}
-    </div>
+    <>
+      <div className="logoContainer"style={{ maxWidth: '60%', margin: '0 auto', display: 'block' }}>
+        <Logo />
+      </div>
+    </>
+
+    
+
+
   );
 
 }
