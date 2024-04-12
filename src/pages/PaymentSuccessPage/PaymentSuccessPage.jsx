f04c206b91653601f49aed1ba16f1629e7740f7c
teyotobias <teyotobias@utexas.edu>
2024-04-11 19:24:10 -0500

    Navbar and sidebar responsive fixes

--- src/pages/PaymentSuccessPage/PaymentSuccessPage.jsx
+++ src/pages/PaymentSuccessPage/PaymentSuccessPage.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { verifySession } from '../../utilities/orders-api';
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 import './PaymentSuccessPage.css';
 // import Logo from '../../components/Logo/Logo';
 export default function PaymentSuccessPage() {
